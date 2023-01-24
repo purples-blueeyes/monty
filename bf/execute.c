2c35aec7d05282c295b735ff3d72096475c1fa05
Anthoine <dzimahanthoine@gmail.com>
2023-01-24 06:20:49 -0800

    " _ "

---
+++ bf/execute.c
@@ -0,0 +1,10 @@
+#include "monty.h"
+
+/**
+ * execute - executes the opcode
+ * @stack: head linked list - stack
+ * @counter: line_counter
+ * @file: poiner to monty file
+ * @content: line content
+ * Return: no return
+ */
