cb104e2871b615ca10328fd5d02e4fa4e91eb259
Anthoine <dzimahanthoine@gmail.com>
2023-02-28 05:54:01 -0800

    monty task

--- free-stack.c
+++ free-stack.c
@@ -17,3 +17,17 @@ void free_stack(stack_t *head)
 		head = aux;
 	}
 }
+
+/**
+* f_stack - function that prints the top
+* @head: head of stack
+* @counter: line count
+*
+* Return: nothing
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
